6db110427066866f49eadbb7a4b7ca2f9bdf43d5
m.sabir <mohammadsabir1q2w@gmail.com>
2026-07-25 20:33:45 +0530

    Added Python practice programs

--- 25.py
+++ 25.py
@@ -1,4 +1,4 @@
 a = int(input("Enter a number: "))
 
-for i in range(1,11):
-    print(a,"*", i, "=", a * i)
+for i in range(1,a + 1):
+    print(i)
--- 26.py
+++
@@ -1,4 +0,0 @@
-a = int(input("Enter a number: "))
-
-for i in range(1,a + 1):
-    print(i)
